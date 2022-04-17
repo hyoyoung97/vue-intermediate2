11cef5b4edbe868bdbbf9357f03820ba12df86a7
hyoyoung97 <hyoyoung97@naver.com>
2022-04-17 16:50:51 +0900

    TodoHeader 기본 스타일 적용, 전체 기본 스타일 적용

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -21,10 +21,22 @@ export default {
     'todo-list' : TodoList,
     'todo-footer' : TodoFooter
   }
-
 }
 </script>
 
 <style>
-
+body {
+  text-align: center;
+  background-color: #F6F6F6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
